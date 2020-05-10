386e2c9eef13573e05af5dd613e307c0d7f0b518
TJ <t_taiwo@live.co.uk>
2020-05-10 12:51:29 +0100

    Reverting to powertrain class. Motors were not powerful enough for the two wheel drive.

--- src/powertrain/powertrain.py
+++ src/powertrain/powertrain.py
@@ -15,12 +15,10 @@ from utils.Motor import degree_calc
 # Turning is relative to if you were looking down onto the robot from above
 directions = {'forward': (0, 1, 0, 1), 'backward': (1, 0, 1, 0),
               'left': (1, 1, 0, 0), 'right': (0, 0, 1, 1),
-              'tots_cw': (0, 0, 0, 0), 'tots_ccw': (1, 1, 1, 1),  # In the following key value pairs only two wheels
+              'tots_cw': (0, 0, 0, 0), 'tots_ccw': (1, 1, 1, 1),
               'diag_fl': (' ', 1, 0, ' '), 'diag_fr': (1, ' ', ' ', 0),
-              # will be driven. Undriven wills will use placeholder
               'diag_rl': (0, ' ', ' ', 1), 'diag_rr': (' ', 0, 1, ' '),
-              # values of 0 for motor direction. Alternative is to
-              'cor_right_cw:': (0, ' ', 0, ' '), 'cor_right_ccw': (1, ' ', 1, ' '),  # to use None.
+              'cor_right_cw:': (0, ' ', 0, ' '), 'cor_right_ccw': (1, ' ', 1, ' '),
               'cor_left_cw:': (' ', 1, ' ', 1), 'cor_left_ccw': (' ', 0, ' ', 0),
               'tur_rear_ax_cw': (0, 0, ' ', ' '), 'tur_rear_ax_ccw': (1, 1, ' ', ' '),
               'tur_front_ax_cw': (' ', ' ', 0, 0), 'tur_front_ax_ccw': (' ', ' ', 1, 1)}
@@ -35,32 +33,16 @@ class Powertrain:
     def go(self, direction='forward', distance=0.1, speed=30, initdelay=.05, verbose=False):
         steps = dist_2_steps_wheel(distance)[0]
         stepdelay = percentage_to_step_delay(speed)
-        #       print(f' calc steps is {steps} and calc stepdelay is {stepdelay}')
-
-        if 'diag' in direction or 'cor' in direction or 'tur' in direction:
-            mod_step_pins = []
-#            print("Not sure how to handle this direction yet")
-            for i, val in enumerate(directions[direction]):
-                if val != ' ':
-                    mod_step_pins.append(self.step_pins[i])
-
+        GPIO.output(self.direction_pins, directions[direction])
         try:
             sleep(initdelay)
             for i in range(steps):
-                if 'diag' in direction or 'cor' in direction or 'tur' in direction:
-                    mod_step_pins = []
-                    print("Not sure how to handle this direction yet")
-                    for j, val in enumerate(directions[direction]):
-                        if val != ' ':
-                            mod_step_pins.append(self.step_pins[j])
-                    #                print('Attempting to do one step')
-                    GPIO.output(mod_step_pins, True)
-                    #                print('Step completed')
-                    sleep(stepdelay)
-                    GPIO.output(mod_step_pins, False)
-                    sleep(stepdelay)
-                    if verbose:
-                        print("Steps count {}".format(i))
+                GPIO.output(self.step_pins, True)
+                sleep(stepdelay)
+                GPIO.output(self.step_pins, False)
+                sleep(stepdelay)
+                if verbose:
+                    print("Steps count {}".format(i))
                 else:
                     GPIO.output(self.step_pins, True)
                     #                print('Step completed')
@@ -76,12 +58,11 @@ class Powertrain:
             print(sys.exc_info()[0])
             print(motor_error)
             print("RpiMotorLib  :(is it here)  Unexpected error:")
-        else:
-            # print report status
+        # print report status
             if verbose:
                 print('\nMotor Run finished, Details:\n')
                 print(f'Direction = {direction}')
-                print(f"Number of steps = {steps}")
+                print(f"Number of steps = {self.steps}")
                 print(f"Step Delay = {stepdelay}")
                 print(f"Initial delay = {initdelay}")
                 print(f"Rotation of wheels in degrees = {degree_calc(steps)}")
